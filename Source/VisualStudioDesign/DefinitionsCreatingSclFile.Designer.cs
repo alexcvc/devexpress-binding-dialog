bf7c6ffad3ea40814f5eea6a1a84cf5434c793b0
Alexander Sacharov <a.sacharov@gmx.de>
2025-08-16 16:57:45 +0200

    Update project configuration and add new components
    - Updated `.gitignore` to include default ignored files.
    - Added `encodings.xml`, `indexLayout.xml`, `misc.xml`, and `vcs.xml` for project settings and configurations.
    - Corrected project path format in `DevExpressBindingDialog.sln`.
    - Created `App.config` for application settings related to DevExpress components.
    - Added new Windows Form files: `DefinitionsCreatingSclFile.Designer.cs`, `DefinitionsCreatingSclFile.cs`, and `DefinitionsCreatingSclFile.resx`.
    - Implemented main entry point in `Program.cs`.
    - Introduced settings management with `Settings.Designer.cs` and `Settings.settings`.
    - Updated `VisualStudioDesign.csproj` to use .NET SDK and added necessary package references.

---
+++ Source/VisualStudioDesign/DefinitionsCreatingSclFile.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace VisualStudioDesign
+{
+    partial class DefinitionsCreatingSclFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
+            simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            dataLayoutControl1 = new DevExpress.XtraDataLayout.DataLayoutControl();
+            layoutControlGroup3 = new DevExpress.XtraLayout.LayoutControlGroup();
+            layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
+            layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            layoutControl2 = new DevExpress.XtraLayout.LayoutControl();
+            layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataLayoutControl1).BeginInit();
+            dataLayoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlItem1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlItem3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)emptySpaceItem1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)emptySpaceItem2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Root).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup1).BeginInit();
+            SuspendLayout();
+            // 
+            // layoutControlGroup2
+            // 
+            layoutControlGroup2.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            layoutControlGroup2.GroupBordersVisible = false;
+            layoutControlGroup2.Location = new System.Drawing.Point(0, 0);
+            layoutControlGroup2.Name = "layoutControlGroup2";
+            layoutControlGroup2.Size = new System.Drawing.Size(342, 277);
+            layoutControlGroup2.TextVisible = false;
+            // 
+            // simpleButton1
+            // 
+            simpleButton1.Location = new System.Drawing.Point(315, 12);
+            simpleButton1.Name = "simpleButton1";
+            simpleButton1.Size = new System.Drawing.Size(137, 22);
+            simpleButton1.StyleController = dataLayoutControl1;
+            simpleButton1.TabIndex = 0;
+            simpleButton1.Text = "simpleButton1";
+            // 
+            // dataLayoutControl1
+            // 
+            dataLayoutControl1.Controls.Add(simpleButton1);
+            dataLayoutControl1.Controls.Add(simpleButton3);
+            dataLayoutControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            dataLayoutControl1.Location = new System.Drawing.Point(0, 225);
+            dataLayoutControl1.Name = "dataLayoutControl1";
+            dataLayoutControl1.Root = layoutControlGroup3;
+            dataLayoutControl1.Size = new System.Drawing.Size(676, 120);
+            dataLayoutControl1.TabIndex = 2;
+            dataLayoutControl1.Text = "dataLayoutControl1";
+            // 
+            // layoutControlGroup3
+            // 
+            layoutControlGroup3.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            layoutControlGroup3.GroupBordersVisible = false;
+            layoutControlGroup3.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] { layoutControlItem1, layoutControlItem3, emptySpaceItem1, emptySpaceItem2 });
+            layoutControlGroup3.Name = "layoutControlGroup3";
+            layoutControlGroup3.Size = new System.Drawing.Size(676, 120);
+            layoutControlGroup3.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            layoutControlItem1.Control = simpleButton1;
+            layoutControlItem1.Location = new System.Drawing.Point(303, 0);
+            layoutControlItem1.Name = "layoutControlItem1";
+            layoutControlItem1.Size = new System.Drawing.Size(141, 100);
+            layoutControlItem1.TextVisible = false;
+            // 
+            // simpleButton3
+            // 
+            simpleButton3.Location = new System.Drawing.Point(523, 12);
+            simpleButton3.Name = "simpleButton3";
+            simpleButton3.Size = new System.Drawing.Size(141, 22);
+            simpleButton3.StyleController = dataLayoutControl1;
+            simpleButton3.TabIndex = 2;
+            simpleButton3.Text = "simpleButton3";
+            // 
+            // layoutControlItem3
+            // 
+            layoutControlItem3.Control = simpleButton3;
+            layoutControlItem3.Location = new System.Drawing.Point(511, 0);
+            layoutControlItem3.Name = "layoutControlItem3";
+            layoutControlItem3.Size = new System.Drawing.Size(145, 100);
+            layoutControlItem3.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            emptySpaceItem1.Location = new System.Drawing.Point(444, 0);
+            emptySpaceItem1.Name = "emptySpaceItem1";
+            emptySpaceItem1.Size = new System.Drawing.Size(67, 100);
+            // 
+            // emptySpaceItem2
+            // 
+            emptySpaceItem2.Location = new System.Drawing.Point(0, 0);
+            emptySpaceItem2.Name = "emptySpaceItem2";
+            emptySpaceItem2.Size = new System.Drawing.Size(303, 100);
+            // 
+            // layoutControl1
+            // 
+            layoutControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            layoutControl1.Location = new System.Drawing.Point(0, 0);
+            layoutControl1.Name = "layoutControl1";
+            layoutControl1.Root = Root;
+            layoutControl1.Size = new System.Drawing.Size(676, 183);
+            layoutControl1.TabIndex = 3;
+            layoutControl1.Text = "layoutControl1";
+            // 
+            // Root
+            // 
+            Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            Root.GroupBordersVisible = false;
+            Root.Name = "Root";
+            Root.Size = new System.Drawing.Size(676, 183);
+            Root.TextVisible = false;
+            // 
+            // layoutControl2
+            // 
+            layoutControl2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            layoutControl2.Location = new System.Drawing.Point(0, 105);
+            layoutControl2.Name = "layoutControl2";
+            layoutControl2.Root = layoutControlGroup1;
+            layoutControl2.Size = new System.Drawing.Size(676, 120);
+            layoutControl2.TabIndex = 4;
+            layoutControl2.Text = "layoutControl2";
+            // 
+            // layoutControlGroup1
+            // 
+            layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            layoutControlGroup1.GroupBordersVisible = false;
+            layoutControlGroup1.Name = "layoutControlGroup1";
+            layoutControlGroup1.Size = new System.Drawing.Size(676, 120);
+            layoutControlGroup1.TextVisible = false;
+            // 
+            // DefinitionsCreatingSclFile
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(676, 345);
+            Controls.Add(layoutControl2);
+            Controls.Add(layoutControl1);
+            Controls.Add(dataLayoutControl1);
+            Name = "DefinitionsCreatingSclFile";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataLayoutControl1).EndInit();
+            dataLayoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlItem1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlItem3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)emptySpaceItem1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)emptySpaceItem2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Root).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup1).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraDataLayout.DataLayoutControl dataLayoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.LayoutControl layoutControl2;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+    }
+}
+
